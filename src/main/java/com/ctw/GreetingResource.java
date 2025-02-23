68dc5e589801506b124d4a4ebf1db74d6856f622
Tales Figueiredo (CTW) <tales.figueiredo@ctw.bmwgroup.com>
2025-02-23 16:23:44 +0000

    Create Quarkus project

---
+++ src/main/java/com/ctw/GreetingResource.java
@@ -0,0 +1,18 @@
+package com.ctw;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.RestQuery;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(@RestQuery String name) {
+        name = name == null ? "World" : name;
+        return "Hello " + name + ", Hi from Quarkus REST";
+    }
+}
